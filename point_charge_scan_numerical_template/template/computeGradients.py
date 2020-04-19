9dbbba7c6e76f08af033fee683f835b5ce29927b
Hymel J H <2h9@or-slurm-login05.ornl.gov>
2020-04-19 04:54:30 -0400

    Revert "Human-readable grad0 made in addition to grad0. Power and dipole spectrum tools added."
    This reverts commit 95e53d27a5cdef973f182e1e06c007d2b9b554fa.

--- point_charge_scan_numerical_template/template/computeGradients.py
+++ point_charge_scan_numerical_template/template/computeGradients.py
@@ -9,26 +9,21 @@
 import os
 import commands
 import sys
-import numpy as np
 
 inputGeom = sys.argv[1]
-stepSize=float(commands.getoutput("sed '3q;d' gradient_estimation.out | awk '{ print $10 }'"))
+stepSize = float(sys.argv[2])
 
 home = os.getcwd()
 
-outputFiles = [f for f in os.listdir(os.path.join(home)) if f.endswith('.out') and f.startswith('input')]
+outputFiles = [f for f in os.listdir(os.path.join(home)) if f.endswith('.out')]
 outputFiles.sort()
 
-l = range(len(outputFiles))
+numberOfFiles = len(outputFiles)-1
+l = range(numberOfFiles)
 energies = []
 atom = []
 axisCount = 0
-
-# This complicated looking zip list is basically grabbing each pair of .out files so that those shifted in opposite (+/-) directions are together,
-# this is necessary to compute the central finite difference. Example (1,2),(3,4),(5,6)...(i,j)
 for i,j in zip(l,l[1:])[::2]:
-	# Determine the correct grep call to make in order to retrieve energies from .out files.
-	# The .lower() command makes it so that differences in case (SCF vs scf vs sCf) in the run file aren't an issue.
 	if 'scf' == str(commands.getoutput("tail -1 gradInput.nw" % i)).split(' ')[1].lower():
 		left = float(commands.getoutput("grep 'Total SCF energy' input%i.out | tail -1 | awk '{print $5}'" % i))
 		right = float(commands.getoutput("grep 'Total SCF energy' input%i.out | tail -1 | awk '{print $5}'" % j))
@@ -48,19 +43,8 @@ with open(inputGeom,'r') as f:
 	atomNames = [x[0] for x in tmpList]
 	print (atomNames)
 
-# Write grad0, the file containing forces that will be read using the read_force flag in the modified NWChem code.
 with open('grad0','w+') as g:
 	for i in energies:
 		for j in i:
 			g.write(str(j))
 			g.write('\n')
-
-# Write a human-readable form of grad0
-hrg = []
-labels = ['Atoms','Force(X)','Force(Y)','Force(Z)']
-hrg.append(labels)
-for lineNumber, line in enumerate(energies):
-	line.insert(0,atomNames[lineNumber])
-	hrg.append(line)
-np.vstack(hrg)
-np.savetxt('human_readable_grad0',hrg,fmt="%s", delimiter=' \t')
